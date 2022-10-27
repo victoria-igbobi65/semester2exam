085d591c6562cfb150bde4aa97fc930a3963dbaf
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-27 21:44:31 +0100

    updated post routes

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const passport = require('passport')
 
+const postController = require('../controllers/post')
 const postRouter = express.Router()
 require('../utils/auth')/*Auth middleware*/
 
 postRouter
     .route('/')
-    .get()
+    .post(passport.authenticate("jwt", {session: false}), postController.createPost)
+    
 
 
 module.exports=postRouter
