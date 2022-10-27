5e52a1879836f43e58d8473ca6784b8a162f7a2f
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-28 00:29:31 +0100

    updated

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post')
+const AppError = require('../utils/appError')
 
 exports.createPost = async(req, res, next) => {
     try{
@@ -25,4 +26,36 @@ exports.createPost = async(req, res, next) => {
             err: err
         })
     }
+}
+
+exports.updatePost = async(req, res, next) => {
+    try{
+        const { id } = req.params;
+        const visitor = req.user._id;
+
+        /*1 Checking if the requested post belongs to the visitor*/
+        const post = await Post.findOne({ _id: id, owner_id: visitor });
+
+        /*2 Error message*/
+        if (!post) {
+        return next(new AppError("Check the post Id and try again!", 400));}
+
+        /*3 Getting data to update*/
+        if (req.body.body) {
+        post.body = req.body.body;}
+        if (req.body.state) {
+        post.state = req.body.state;}
+
+        post.save({ validateBeforeSave: true });
+        return res.status(200).json({
+        status: true,
+        post: post})
+    }
+    catch(err){
+        return res.status(500).json({
+            status: false,
+            err: err
+        })
+    }
+
 }
