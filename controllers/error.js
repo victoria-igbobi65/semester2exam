a324189bee712494692c9ae0c6c57d50cb386e29
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-28 22:53:58 +0100

    updated global error handler

--- controllers/error.js
+++ controllers/error.js
@@ -1,11 +1,36 @@
-module.exports = (err, req, res, next) => {
-  console.log(err);
+require('dotenv').config()
+
+const prodValidationError = err => {
+  console.log('Hello')
+}
+
+const devHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const status = err.status || "error";
   res.status(statusCode).json({
     status: status,
     message: err.message,
 
-    //stack: err.stack
   });
-};
+}
+
+var prodHandler = (err, req, res, next) => {
+  if (err.name === 'ValidationError') {err = prodValidationError(err)}
+
+  if (err.isOperational){
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    })
+  }
+
+  else {
+    return res.status(500).json({
+      status: "error",
+      message: "Server Issues!",
+    });
+  }
+}
+
+
+module.exports = process.env.NODE_ENV === 'development' ? devHandler: prodHandler
