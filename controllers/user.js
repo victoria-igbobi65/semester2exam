9d209e015cfc6e21039b323023ace4f23b8c765e
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-31 00:03:15 +0100

    updated user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,53 +1,42 @@
 const User = require("../models/user");
 const AppError = require("../utils/appError");
+const catchAsync = require('../utils/catchAsync')
 const helpers = require("../utils/helpers");
 
 
-exports.createUser = async (req, res, next) => {
-  try {
-    console.log(req.body)
-    const { first_name, last_name, email, password } = req.body;
+exports.createUser = catchAsync( async (req, res, next) => {
 
-    const newUser = await User.create({
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      password: password,
-    });
+  const { first_name, last_name, email, password } = req.body;
 
-    res.status(201).json({ status: true, newUser: newUser });
-  } catch (err) {
-    res.status(500).json({
-      status: false,
-      err: err,
-    });
+  /*Save new user to DB*/
+  const newUser = await User.create({
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    password: password,
+  });
+
+  /*Success response*/
+  res.status(201).json({ status: true, newUser: newUser });
+  
+});
+
+
+exports.login = catchAsync( async (req, res, next) => {
+
+  const { email, password } = req.body;
+
+  /*Find user and check if user*/
+  const user = await User.findOne({ email }).select("+password");
+  if (!user || !(await user.correctPassword(password))) {
+    return next(new AppError("Incorrect email or password!", 401));
   }
-};
 
-
-exports.login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-
-      /*Find user*/
-    const user = await User.findOne({ email }).select("+password")
-
-      /*check if user is not found or password incorrect*/
-    if (!user || !(await user.correctPassword(password))) {
-      return next(new AppError("Incorrect email or password!", 401));
-    }
-
-    /*Generate token for user*/
-    const token = helpers.signToken(user._id);
-
-    return res.status(200).json({
-      status: true,
-      token,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: false,
-      err: err,
-    });
-  }
-};
+  /*Generate token for user and send token to user*/
+  const token = helpers.signToken(user._id);
+  return res.status(200).json({
+    status: true,
+    token,
+  });
+  
+});
