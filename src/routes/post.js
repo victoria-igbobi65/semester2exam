5dd975d251bcd3a0351788941f5a5a594cbd7e6c
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-11-03 19:56:29 +0100

    restructured user routes

---
+++ src/routes/post.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const passport = require('passport')
+
+const postController = require('../controllers/post')
+const postRouter = express.Router()
+const auth = require('../middlewares/protect')
+require('../middlewares/auth')/*Auth middleware*/
+
+
+postRouter
+    .route('/state/:id')
+    .patch(passport.authenticate("jwt", { session: false }), auth.protect, postController.updateState)
+
+postRouter
+    .route('/')
+    .post(passport.authenticate("jwt", { session: false }), postController.createPost)
+    .get(postController.getAllPost)
+
+
+postRouter
+    .route("/me")
+    .get(passport.authenticate("jwt", { session: false }), postController.getAllMyPost);
+
+
+postRouter
+    .route("/:id")
+    .patch(passport.authenticate("jwt", { session: false }), auth.protect, postController.updatePost)
+    .delete(passport.authenticate("jwt", { session: false }),auth.protect, postController.deletePost)
+    .get(postController.getPostById) 
+
+
+module.exports=postRouter
